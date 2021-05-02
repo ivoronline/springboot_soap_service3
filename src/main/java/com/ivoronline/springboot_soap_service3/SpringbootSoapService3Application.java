cff56dc33edd7b24711cbbf91ffe0f9e4ebcd9a4
igoreta <ivoronline@gmail.com>
2021-05-02 16:50:01 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_soap_service3/SpringbootSoapService3Application.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_soap_service3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSoapService3Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootSoapService3Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_soap_service3/SpringbootSoapService3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_soap_service3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSoapService3ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
